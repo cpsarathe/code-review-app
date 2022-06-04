7e3cb27b483f5445a7dca9b3c72013a2084bd097
cpemaratech <Chandra.Sarathe@digitaleconomysolutions.com>
2022-06-04 13:11:07 +0400

    Basic spring boot app to use for code review

--- src/main/java/com/code/review/app/controller/CustomerController.java
+++ src/main/java/com/code/review/app/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,7 +20,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping(value = "/register")
+    @PostMapping(value = "/register" , produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> registerCustomer(@RequestBody CustomerDTO customerDTO) {
         customerService.register(customerDTO);
@@ -31,6 +32,19 @@ public class CustomerController {
     public ResponseEntity<CustomerDTO> getAllCustomers() {
         List<CustomerDTO> customerDTOList = customerService.getAllCustomers();
         return new ResponseEntity(customerDTOList,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/search" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<CustomerDTO> getAllCustomersByEmail(@RequestParam(name = "email") String email) {
+        List<CustomerDTO> customerDTOList = customerService.getAllCustomers();
+        List<CustomerDTO> resultCustomers = new ArrayList<>();
+        for(CustomerDTO customerDTO : customerDTOList) {
+            if(customerDTO.getEmail().equals(email)) {
+                resultCustomers.add(customerDTO);
+            }
+        }
+        return new ResponseEntity(resultCustomers,HttpStatus.OK);
     }
 
 }
